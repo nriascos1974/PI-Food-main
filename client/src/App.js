81b1fcf65541da733c18444fe0e68d689b969c33
nriascos1974 <nestorriascos74@gmail.com>
2023-04-06 01:39:36 -0500

    Front Version 1.0.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes, useLocation } from "react-router-dom";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Home from "./components/Home/Home";
+import Detail from "./components/DetailRecipe/Detail";
+import NewRecipe from "./components/NewRecipe/NewRecipe";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/detail/:detailId" element={<Detail/>} />
+        <Route path="/recipe" element={<NewRecipe />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
